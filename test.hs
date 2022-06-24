4842900275a0e154b1208f36e78103f143476b85
lim6112j <lim6112j@gmail.com>
2022-06-25 01:59:34 +0900

    pattern matching

--- test.hs
+++ test.hs
@@ -6,13 +6,18 @@ toCamelCase :: String -> String
 toCamelCase = foldr func acc
   where
     acc = []
-    func x []
-      | x == '-' = []
-      | otherwise = [x]
-    func x acc@(h : xs)
-      | x == '-' = toUpper h : xs
-      | x == '_' = toUpper h : xs
-      | otherwise = x : acc
+    func '-' [] = []
+    func x [] = [x]
+    func '-' acc@(h:xs) = toUpper h:xs
+    func '_' acc@(h:xs) = toUpper h:xs
+    func x s@(h:xs) = x : s 
+    -- func x []
+      -- | x == '-' = []
+      -- | otherwise = [x]
+    -- func x acc@(h : xs)
+      -- | x == '-' = toUpper h : xs
+      -- | x == '_' = toUpper h : xs
+      -- | otherwise = x : acc
 
 --func = \x acc -> case acc of
 --[] -> case x of
